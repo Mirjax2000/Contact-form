f09f2c69877a976d6d7eb55ac0d31e7d344b3bc1
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-01-07 20:58:16 +0100

    start js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,24 @@
 // $(function () {});
 // -----------------
 
-$(function () {
-    var testing = $('.testing'),
-        bottom = $('.bottom'),
-        result = $('.result'),
-        top = $('.top');
-
-    top.children().on('click', function () {
-        thys = $(this);
-        result.append(thys.text());
-    });
-});
+(function () {
+  const form = $("#form"),
+    first_name = $("#first_name"),
+    last_name = $("#last_name"),
+    email_address = $("#email_address"),
+    checkbox_general = $("#general"),
+    checkbox_support = $("#support"),
+    checkbox_consent = $("#consent"),
+    message = $("#message"),
+    error = $(".form__error");
+  console.log(
+    first_name,
+    last_name,
+    email_address,
+    checkbox_general,
+    checkbox_support,
+    checkbox_consent,
+    checkbox_consent,
+    message,
+  );
+})(jQuery);
