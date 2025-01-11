787c0771db83c11984127c2ebf9782cf49fb9cbd
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-01-11 12:29:21 +0100

    all css

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
 // $(function () {});
 // -----------------
 
+(() => {
+  const form = document.getElementById("contactForm"),
+    modal = document.getElementById("modal"),
+    button = form.querySelector(".form__submit"),
+    firstName = form.querySelector("#first_name"),
+    lastName = form.querySelector("#last_name"),
+    checkboxGeneral = form.querySelector("#general"),
+    checkboxSupport = form.querySelector("#support");
+})();
