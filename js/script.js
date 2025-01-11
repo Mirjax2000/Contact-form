03e374f0be93639693d2e1fbf9bb6b23a512be40
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-01-11 15:58:28 +0100

    all css

--- js/script.js
+++ js/script.js
@@ -7,9 +7,31 @@
 (() => {
   const form = document.getElementById("contactForm"),
     modal = document.getElementById("modal"),
-    button = form.querySelector(".form__submit"),
-    firstName = form.querySelector("#first_name"),
-    lastName = form.querySelector("#last_name"),
-    checkboxGeneral = form.querySelector("#general"),
-    checkboxSupport = form.querySelector("#support");
+    general = form.querySelector("#general"),
+    support = form.querySelector("#support"),
+    checkboxesArray = [general, support];
+
+  checkboxesArray.forEach((checkbox) => {
+    checkbox.addEventListener("click", (e) => {
+      const current = e.target;
+      if (current.checked) {
+        checkboxesArray.forEach((box) => {
+          box.removeAttribute("required");
+          box.checked = false;
+          current.checked = true;
+          current.setAttribute("required", "");
+        });
+      } else {
+        checkboxesArray.forEach((box) => {
+          box.setAttribute("required", "");
+        });
+      }
+    });
+  });
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (form.checkValidity()) {
+      modal.classList.remove("hide");
+    }
+  });
 })();
